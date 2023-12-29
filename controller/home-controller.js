c09a8da6a9822047866838e87a5e1101716b58b4
chu trinh <chutrinh@gmail.com>
2023-12-29 18:39:08 +0700

    third

--- controller/home-controller.js
+++ controller/home-controller.js
@@ -3,7 +3,7 @@ const Macbook = require("../model/macbook-model");
 exports.macbook = async (req, res, next) => {
   try {
     const macBook = await Macbook.find();
-    res.json(macBook);
+    res.json({ states: "ok", data: macBook });
   } catch (error) {
     res.json(error);
   }
