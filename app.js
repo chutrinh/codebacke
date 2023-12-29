466bd42848925a874db62374eea4f32ba4fdec49
chu trinh <chutrinh@gmail.com>
2023-12-29 18:27:16 +0700

    second

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
+app.use(cors());
 
 const homeRounter = require("./router/home-router");
 app.use("/home", homeRounter);
